b38ad90d44fccbf6c31f5b0b2dc061024b481f61
Sophia Maria <sophitmgc3115@gmail.com>
2026-05-15 13:36:06 -0300

    Correções

--- receitas/views.py
+++ receitas/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from utils.receitas.factory import make_receitas
+from utils.receitas.factory import make_receita
 
 # Create your views here.
 
 #HTTP REQUEST
 def home(request):
     return render(request, 'receitas/pages/home.html', context={
-        'receitas': [make_receitas() for _ in range(10)],
+        'receitas': [make_receita() for _ in range(10)],
     })
 
 def receita(request, id):
-    return render(request, 'receitas/pages/receitas-view', context={
-        'recipe': make_receitas(),
+    return render(request, 'receitas/pages/receita-view.html', context={
+        'receita': make_receita(),
     })
 
 
